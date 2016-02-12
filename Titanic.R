620ada00b5b95fe3908da4744e0382a103f55588
Michael Hunt <mike038@btinternet.com>
2016-02-12 14:55:36 +0000

    made it worse

--- Titanic.R
+++ Titanic.R
@@ -1,57 +1,99 @@
 ## Titanic Kaggle
 
-dataTrain<-read.csv("./data/train.csv")
-dataTest<-read.csv("./data/test.csv")
-str(dataTrain)
-summary(dataTrain)
+mbh<-function(){
+  print ("Hello World")
+}
+  set.seed(1)
+  rm(list=ls())
+  dataTrain<-read.csv("./data/train.csv")
+  dataTest<-read.csv("./data/test.csv")
+  #str(dataTrain)
+  #summary(dataTrain)
+  
+   dataTrain<-dataTrain[complete.cases(dataTrain),]
+  # impute na to median
+  dataTrain$Age[is.na(dataTrain$Age)] =median(dataTrain$Age, na.rm=TRUE)
+  dataTest$Age[is.na(dataTest$Age)] =median(dataTest$Age, na.rm=TRUE)
+  dataTest$Fare[is.na(dataTest$Fare)] =median(dataTest$Fare, na.rm=TRUE)
+  
+  dataTest$Cabin<-as.factor(dataTest$Cabin)
+  dataTrain$Cabin<-as.factor(dataTrain$Cabin)
+  
+  dataTrain$Cabin<-as.character(dataTrain$Cabin)
+  dataTrain$Cabin<-substr(dataTrain$Cabin, 1, 1)
+  #dataTrain$Cabin<-as.factor(dataTrain$Cabin)
+  
+  for (i in 1:length(dataTrain$Cabin)){
+    if (dataTrain$Cabin[i]=="") dataTrain$Cabin[i]="H"
+  }
+  
+  
+  dataTest$Cabin<-as.character(dataTest$Cabin)
+  dataTest$Cabin<-substr(dataTest$Cabin, 1, 1)
+  #dataTest$Cabin<-as.factor(dataTest$Cabin)
+  
+  for (i in 1:length(dataTest$Cabin)){
+    if (dataTest$Cabin[i]=="") dataTest$Cabin[i]="H"
+    if (dataTest$Cabin[i]=="G") dataTest$Cabin[i]="H"
+  }
 
-#dataTrain<-dataTrain[complete.cases(dataTrain),]
-# impute na to median
-dataTrain$Age[is.na(dataTrain$Age)] =median(dataTrain$Age, na.rm=TRUE)
-dataTest$Age[is.na(dataTest$Age)] =median(dataTest$Age, na.rm=TRUE)
-dataTest$Fare[is.na(dataTest$Fare)] =median(dataTest$Fare, na.rm=TRUE)
+  
+  # set.seed(1000)
+  # train_index=sample(seq(nrow(dataTrain)),418,replace=FALSE)
+  # train=dataTrain[train_index,]
+  # test=dataTrain[-train_index,]
+  
+  
+  library(caTools)
+  set.seed(1000)
+  spl = sample.split(dataTrain$Survived, SplitRatio = 0.5)
+  train = subset(dataTrain, spl==TRUE)
+  test = subset(dataTrain, spl==FALSE)
+  
+  train$Cabin[271]="H"
+  dataTest$Cabin[118]="H"
+  
+#   # train on one gender at a time
+#   train<-subset(train,Sex=="male")
+#   #train$Sex<-factor(train$Sex)
+#   test<-subset(test,Sex=="male")
+#   #test$Sex<-factor(test$Sex)
+#   dataTest<-subset(dataTest,Sex=="male")
+#   #dataTest$Sex<-factor(dataTest$Sex)
 
-dataTrain$Cabin<-as.character(dataTrain$Cabin)
-dataTrain$Cabin<-substr(dataTrain$Cabin, 1, 1)
-#dataTrain$Cabin<-as.factor(dataTrain$Cabin)
+  train$Survived<-as.factor(train$Survived)
+  test$Survived<-as.factor(test$Survived)
+  #train$Cabin<-as.factor(train$Cabin)
+  #test$Cabin<-as.factor(test$Cabin)
+  #train$Cabin<-factor(train$Cabin)
+  #test$Cabin<-factor(test$Cabin)
+  #levels(test$Cabin) <- levels(train$Cabin)
+  
+## Models
 
-for (i in 1:length(dataTrain$Cabin)){
-  if (dataTrain$Cabin[i]=="") dataTrain$Cabin[i]="H"
+model<-function(x){
+md=list(5)  
+md[[1]]<-as.formula("Survived~Pclass+Fare")
+md[[2]]<-as.formula("Survived~Pclass+Fare+Age")
+md[[3]]<-as.formula("Survived~Pclass+Fare+Age+SibSp+Parch") # Age na rows removed
+md[[4]]<-as.formula("Survived~Pclass+Fare+Age")
+md[[5]]<-as.formula("Survived~Pclass+Fare+Age+SibSp+Parch+Ticket")
+md[[x]]
 }
 
 
-dataTest$Cabin<-as.character(dataTest$Cabin)
-dataTest$Cabin<-substr(dataTest$Cabin, 1, 1)
-#dataTest$Cabin<-as.factor(dataTest$Cabin)
-
-for (i in 1:length(dataTest$Cabin)){
-  if (dataTest$Cabin[i]=="") dataTest$Cabin[i]="H"
-}
-
-
-# set.seed(1000)
-# train_index=sample(seq(nrow(dataTrain)),418,replace=FALSE)
-# train=dataTrain[train_index,]
-# test=dataTrain[-train_index,]
-
-library(caTools)
-set.seed(1000)
-spl = sample.split(dataTrain$Survived, SplitRatio = 0.8)
-train = subset(dataTrain, spl==TRUE)
-test = subset(dataTrain, spl==FALSE)
-
-train$Cabin[271]="H"
 
 # logistic Regression model
+
+for (j in 1:4){
+#Tstart()
 train$Survived<-as.factor(train$Survived)
 test$Survived<-as.factor(test$Survived)
-fol=formula(Survived~Pclass+Sex+Fare)
-fol2=formula(Survived~Pclass+Sex+Fare+Age)
-fol4=formula(Survived~Pclass+Sex+Fare+Age+Cabin+SibSp+Parch)
-glm.fit=glm(fol4,data=train,family=binomial)
+
+glm.fit=glm(model(j),data=train,family=binomial)
 glm.probs<- predict(glm.fit, newdata = test, type = "response")
 
-
+# find optimum threshold
 maxThreshold<-0
 glm.accMax<-0
 thresholds<-seq(.2, .9, length.out=50 )
@@ -68,8 +110,8 @@ for (i in 1:length(thresholds)){
 plot(thresholds,glm.acc)
 glm.pred=rep(0,length(test$Survived))
 glm.pred[glm.probs>maxThreshold]=1
-sum(glm.pred==test$Survived)/nrow(test)
-
+print (sum(glm.pred==test$Survived)/nrow(test))
+}
 
 glm.probs<- predict(glm.fit, newdata = dataTest, type = "response")
 glm.pred=rep(0,nrow(dataTest))
@@ -80,27 +122,28 @@ names(submission)<-c("PassengerId","Survived")
 write.table(submission,"./submissions/glm2_imputed.csv",sep=",",row.names=FALSE)
 
 # Decision tree using CART
+
+
 library(rpart)
 library(rpart)
 library(rpart.plot)
 
-fol1=formula(Survived~Pclass+Sex+Fare)
-fol2=formula(Survived~Pclass+Sex+Fare+Age) # Age na rows removed
-fol3=formula(Survived~Pclass+Sex+Fare+Age+Cabin) # Age na rows removed
-fol4=formula(Survived~Pclass+Sex+Fare+Age+Cabin+SibSp+Parch) # Age na rows removed
-CARTmodel <- rpart(fol4, method="class", data=train)
-print(CARTmodel)
+for (i in 1:5){
+#Tstart()
+CARTmodel <- rpart(model(i), method="class", data=train)
+#print(CARTmodel)
 prp(CARTmodel)
 
 #accuracy of CART model on test set
 PredictCARTmodel = predict(CARTmodel, newdata = test, type = "class")
-sum(PredictCARTmodel==test$Survived)/nrow(test)
+print (sum(PredictCARTmodel==test$Survived)/nrow(test))
+}
 
 #submit
 CART.pred<-predict(CARTmodel, newdata = dataTest, type = "class")
 submission<-data.frame(dataTest$PassengerId,as.numeric(CART.pred)-1)
 names(submission)<-c("PassengerId","Survived")
-write.table(submission,"./submissions/cart4_impute.csv",sep=",",row.names=FALSE)
+write.table(submission,"./submissions/cart3_impute.csv",sep=",",row.names=FALSE)
 
 # CART with cross validation
 ## SELECTING CP BY CROSS-VALIDATION
@@ -122,10 +165,9 @@ tr.control = trainControl(method = "cv", number = 10)
 cartGrid = expand.grid( .cp = seq(0.001,0.1,0.001))
 
 # Cross-validation
+#start()
 train$Survived<-as.factor(train$Survived)
 test$Survived<-as.factor(test$Survived)
-fol4=formula(Survived~Pclass+Sex+Fare+Age+Cabin+SibSp+Parch) # Age na rows removed
-fol5=formula(Survived~Pclass+Sex+Fare+Age+Cabin+Ticket) # Age na rows removed
 tr = train(fol5, data = train, method = "rpart", trControl = tr.control, tuneGrid = cartGrid)
 tr
 
@@ -136,74 +178,88 @@ prp(best.tree)
 # fit CART model with optimal cp = 0.006
 library(rpart)
 library(rpart.plot)
+train$Survived<-as.factor(train$Survived)
+test$Survived<-as.factor(test$Survived)
 
-CARTcp = rpart(fol5, data=train, method="class",cp=0.006)
+for (i in 1:5){
+#Tstart()
+CARTcp = rpart(model(i), data=train, method="class",cp=0.006)
 prp(CARTcp)
 
 # 2.4 accuracy
+
 PredictCARTcp = predict(CARTcp, newdata = test, type = "class")
-sum(PredictCARTcp==test$Survived)/nrow(test)
+print (sum(PredictCARTcp==test$Survived)/nrow(test))
+}
+
+CARTcp = rpart(model(2), data=train, method="class",cp=0.006)
+prp(CARTcp)
+PredictCARTcp = predict(CARTcp, newdata = test, type = "class")
+print (sum(PredictCARTcp==test$Survived)/nrow(test))
 
 #submit
 PredictCARTcp<-predict(CARTcp, newdata = dataTest, type = "class")
 submission<-data.frame(dataTest$PassengerId,as.numeric(PredictCARTcp)-1)
 names(submission)<-c("PassengerId","Survived")
-write.table(submission,"./submissions/cart4_impute_cv.csv",sep=",",row.names=FALSE)
+write.table(submission,"./submissions/cart2_impute_cv.csv",sep=",",row.names=FALSE)
 
 # Random forest model
+
 library(randomForest)
 library(caret)
 library(rpart)
 library(tree)
 library(randomForest)
+accbest=0
+ibest=0
+for (i in 1:4){
+#Tstart()
 train$Survived<-as.factor(train$Survived)
 test$Survived<-as.factor(test$Survived)
-train$Cabin<-as.factor(train$Cabin)
-test$Cabin<-as.factor(test$Cabin)
-fol2<-formula(Survived~Pclass+Sex+Fare+Age)
-fol3<-formula(Survived~Pclass+Sex+Fare+Age+Cabin)
-forestmodel<-randomForest(fol2, data=train)
+forestmodel<-randomForest(model(i), data=train)
 
 rf.probs = predict(forestmodel, newdata = test)
-sum(rf.probs==test$Survived)/nrow(test)
+acc=sum(rf.probs==test$Survived)/nrow(test)
+print (acc)
+if (acc > accbest){
+  accbest=acc
+  ibest =i
+}
 importance(forestmodel)
+}
 
 #submit
+#train$Cabin<-factor(train$Cabin)
 dataTest$Cabin<-as.factor(dataTest$Cabin)
+levels(dataTest$Cabin)<-levels(train$Cabin)
+forestmodel<-randomForest(model(ibest), data=train)
 rf.pred<-predict(forestmodel, newdata = dataTest)
-submission<-data.frame(dataTest$PassengerId,as.numeric(rf.pred)-1)
+submission<-data.frame(dataTest$PassengerId,rf.pred)
 names(submission)<-c("PassengerId","Survived")
-write.table(submission,"./submissions/rf2_impute.csv",sep=",",row.names=FALSE)
+write.table(submission,"./submissions/rf3_f_impute.csv",sep=",",row.names=FALSE)
+
+males<-read.csv("./submissions/rf3_m_impute.csv")
+females<-read.csv("./submissions/rf3_f_impute.csv")
+
+mf<-rbind(males,females)
+mf <- mf[order(mf$PassengerId),] 
+write.table(mf,"./submissions/rf3_mf_impute.csv",sep=",",row.names=FALSE)
+
+mean(mf$Survived)
 
 # SVM Model
+
 library(e1071)
-fol1=formula(Survived~Pclass+Sex+Fare)
-fol2=formula(Survived~Pclass+Sex+Fare+Age)
-fol3<-formula(Survived~Pclass+Sex+Fare+Age+Cabin)
-SVMmodel <- svm(fol3, data=train)
+
+for (j in 1:5){
+#start()
+SVMmodel <- svm(model(j), data=train)
 svm.probs = predict(SVMmodel, newdata = test)
-
-svm.pred=rep(0,length(test$Survived))
-svm.pred[svm.probs>0.5]=1
-sum(svm.pred==test$Survived)/nrow(test)
-
-maxThreshold<-0
-svm.accMax<-0
-thresholds<-seq(0, 1, length.out=100 )
-svm.acc=rep(0,length(thresholds))
-for (i in 1:length(thresholds)){
-  svm.pred=rep(0,length(test$Survived))
-  svm.pred[svm.probs>thresholds[i]]=1
-  svm.acc[i]<-sum(svm.pred==test$Survived)/nrow(test)
-  if (svm.acc[i] > svm.accMax) {
-    svm.accMax = svm.acc[i]
-    maxThreshold=thresholds[i]
-  }
+svm.pred=rep(0,length(svm.probs))
+#svm.pred[svm.probs>0.5]=1
+print (sum(svm.pred==test$Survived)/nrow(test))
 }
-plot(thresholds,svm.acc)
-svm.pred=rep(0,length(test$Survived))
-svm.pred[svm.probs>maxThreshold]=1
-sum(svm.pred==test$Survived)/nrow(test)
+
 
 #submit
 #dataTest$Survived=rep(0,length(nrow(dataTest)))
